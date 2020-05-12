a6647f46a4454db7e94d2a51de023b4c4db151ed
nathan.eckert <nathan.eckert@polytechnique.edu>
2020-05-12 11:30:16 +0200

    [Test]
    Implement solution of Broyden test cases

--- tests/common/broyden1965.rs
+++ tests/common/broyden1965.rs
@@ -28,14 +28,74 @@ extern crate nalgebra;
 pub fn init_broyden1965_case5() -> nalgebra::DVector<f64> {
     nalgebra::DVector::from_vec(vec![-1.0; 5])
 }
+pub fn solution_broyden1965_case5() -> nalgebra::DVector<f64> {
+    let mut solution = nalgebra::DVector::zeros(5);
+    solution[4] = 15.0 - 235.0_f64.sqrt(); // approx -0.32970971675589134
+    solution[3] = 15.0 - (235.0-20.0*solution[4]).sqrt(); // approx -0.5433006255144477
+    solution[2] = 15.0 - (235.0-20.0*solution[3]).sqrt(); // approx -0.6801151944202548
+    solution[1] = 15.0 - (235.0-20.0*solution[2]).sqrt(); // approx -0.5433006255144477
+    solution[0] = 15.0 - (235.0-20.0*solution[1]).sqrt(); // approx -0.32970971675589134
+    solution
+}
+
+
 pub fn init_broyden1965_case6() -> nalgebra::DVector<f64> {
     nalgebra::DVector::from_vec(vec![-1.0; 5])
 }
+pub fn solution_broyden1965_case6() -> nalgebra::DVector<f64> {
+    let mut solution = nalgebra::DVector::zeros(5);
+    solution[4] = 3.0 - 11_f64.sqrt(); // approx -0.3166247903553998
+    solution[3] = 3.0 - (11.0 - 4.0*solution[4]).sqrt(); // approx -0.5023562299431505
+    solution[2] = 3.0 - (11.0 - 4.0*solution[3]).sqrt(); // approx -0.606858039869687
+    solution[1] = 3.0 - (11.0 - 4.0*solution[2]).sqrt(); // approx -0.6643460752880244
+    solution[0] = 3.0 - (11.0 - 4.0*solution[1]).sqrt(); // approx -0.6955898448220816
+    solution
+}
+
 pub fn init_broyden1965_case7() -> nalgebra::DVector<f64> {
     nalgebra::DVector::from_vec(vec![-1.0; 10])
 }
+pub fn solution_broyden1965_case7() -> nalgebra::DVector<f64> {
+    let mut solution = nalgebra::DVector::zeros(10);
+    solution[9] = 3.0 - 11_f64.sqrt(); // approx -0.3166247903553998
+    solution[8] = 3.0 - (11.0 - 4.0*solution[9]).sqrt(); // approx -0.5023562299431505
+    solution[7] = 3.0 - (11.0 - 4.0*solution[8]).sqrt(); // approx -0.606858039869687
+    solution[6] = 3.0 - (11.0 - 4.0*solution[7]).sqrt(); // approx -0.6643460752880244
+    solution[5] = 3.0 - (11.0 - 4.0*solution[6]).sqrt(); // approx -0.6955898448220816
+    solution[4] = 3.0 - (11.0 - 4.0*solution[5]).sqrt(); // approx -0.7124600171972664
+    solution[3] = 3.0 - (11.0 - 4.0*solution[4]).sqrt(); // approx -0.7215373259970219
+    solution[2] = 3.0 - (11.0 - 4.0*solution[3]).sqrt(); // approx -0.726412390488751
+    solution[1] = 3.0 - (11.0 - 4.0*solution[2]).sqrt(); // approx -0.7290279647590476
+    solution[0] = 3.0 - (11.0 - 4.0*solution[1]).sqrt(); // approx -0.7304305192613079
+    solution
+}
+
 pub fn init_broyden1965_case8() -> nalgebra::DVector<f64> {
     nalgebra::DVector::from_vec(vec![-1.0; 20])
+}
+pub fn solution_broyden1965_case8() -> nalgebra::DVector<f64> {
+    let mut solution = nalgebra::DVector::zeros(20);
+    solution[19] = 3.0 - 11_f64.sqrt(); // approx -0.3166247903553998
+    solution[18] = 3.0 - (11.0 - 4.0*solution[19]).sqrt(); // approx -0.5023562299431505
+    solution[17] = 3.0 - (11.0 - 4.0*solution[18]).sqrt(); // approx -0.606858039869687
+    solution[16] = 3.0 - (11.0 - 4.0*solution[17]).sqrt(); // approx -0.6643460752880244
+    solution[15] = 3.0 - (11.0 - 4.0*solution[16]).sqrt(); // approx -0.6955898448220816
+    solution[14] = 3.0 - (11.0 - 4.0*solution[15]).sqrt(); // approx -0.7124600171972664
+    solution[13] = 3.0 - (11.0 - 4.0*solution[14]).sqrt(); // approx -0.7215373259970219
+    solution[12] = 3.0 - (11.0 - 4.0*solution[13]).sqrt(); // approx -0.726412390488751
+    solution[11] = 3.0 - (11.0 - 4.0*solution[12]).sqrt(); // approx -0.7290279647590476
+    solution[10] = 3.0 - (11.0 - 4.0*solution[11]).sqrt(); // approx -0.7304305192613079
+    solution[9]  = 3.0 - (11.0 - 4.0*solution[10]).sqrt(); // approx -0.7311823966465685
+    solution[8]  = 3.0 - (11.0 - 4.0*solution[9]).sqrt();  // approx -0.7315853985385723
+    solution[7]  = 3.0 - (11.0 - 4.0*solution[8]).sqrt();  // approx -0.7318013872866129
+    solution[6]  = 3.0 - (11.0 - 4.0*solution[7]).sqrt();  // approx -0.7319171412487782
+    solution[5]  = 3.0 - (11.0 - 4.0*solution[6]).sqrt();  // approx -0.7319791753163778
+    solution[4]  = 3.0 - (11.0 - 4.0*solution[5]).sqrt();  // approx -0.7320124197630307
+    solution[3]  = 3.0 - (11.0 - 4.0*solution[4]).sqrt();  // approx -0.7320302355490265
+    solution[2]  = 3.0 - (11.0 - 4.0*solution[3]).sqrt();  // approx -0.7320397830403826
+    solution[1]  = 3.0 - (11.0 - 4.0*solution[2]).sqrt();  // approx -0.7320448995371867
+    solution[0]  = 3.0 - (11.0 - 4.0*solution[1]).sqrt();  // approx -0.7320476414628936
+    solution
 }
 
 pub fn broyden1965_case5(x: &nalgebra::DVector<f64>) -> nalgebra::DVector<f64> {
@@ -77,6 +137,12 @@ fn broyden1965_cases5to8(
 pub fn init_broyden1965_case9() -> nalgebra::DVector<f64> {
     nalgebra::DVector::from_vec(vec![-1.2, 1.0])
 }
+pub fn solution_broyden1965_case9() -> nalgebra::DVector<f64> {
+    let mut solution = nalgebra::DVector::zeros(2);
+    solution[0] = 1.0;
+    solution[1] = 1.0;
+    solution
+}
 
 pub fn broyden1965_case9(x: &nalgebra::DVector<f64>) -> nalgebra::DVector<f64> {
     let mut outputs = nalgebra::DVector::zeros(2);
@@ -90,6 +156,12 @@ pub fn broyden1965_case9(x: &nalgebra::DVector<f64>) -> nalgebra::DVector<f64> {
 
 pub fn init_broyden1965_case10() -> nalgebra::DVector<f64> {
     nalgebra::DVector::from_vec(vec![15.0, -2.0])
+}
+pub fn solution_broyden1965_case10() -> nalgebra::DVector<f64> {
+    let mut solution = nalgebra::DVector::zeros(2);
+    solution[0] = 5.0;
+    solution[1] = 4.0;
+    solution
 }
 
 pub fn broyden1965_case10(x: &nalgebra::DVector<f64>) -> nalgebra::DVector<f64> {
--- tests/solver/broyden.rs
+++ tests/solver/broyden.rs
@@ -14,14 +14,17 @@ fn broyden_case5() {
 
     rf.solve(&mut user_model);
 
-    let mut solution = nalgebra::DVector::zeros(problem_size);
-    solution[0] = -0.8222168603829406;
-    solution[1] = -0.7671273188457487;
-    solution[2] = -0.6801151944285924;
-    solution[3] = -0.5433006255196431;
-    solution[4] = -0.32970971675722843;
+    let solution = solution_broyden1965_case5();
 
-    assert_eq!(user_model.get_iteratives(), solution);
+
+    for i in 0..problem_size {
+        assert!(float_cmp::approx_eq!(
+            f64,
+            user_model.get_iteratives()[i],
+            solution[i],
+            epsilon = 1e-6
+        ));
+    }
 
 }
 
@@ -34,14 +37,17 @@ fn broyden_case6() {
 
     rf.solve(&mut user_model);
 
-    let mut solution = nalgebra::DVector::zeros(problem_size);
-    solution[0] = -0.6955898448220836;
-    solution[1] = -0.664346075288027;
-    solution[2] = -0.6068580398696891;
-    solution[3] = -0.5023562299431517;
-    solution[4] = -0.31662479035539975;
+    let solution = solution_broyden1965_case6();
 
-    assert_eq!(user_model.get_iteratives(), solution);
+
+    for i in 0..problem_size {
+        assert!(float_cmp::approx_eq!(
+            f64,
+            user_model.get_iteratives()[i],
+            solution[i],
+            epsilon = 1e-6
+        ));
+    }
 
 
 }
@@ -55,19 +61,16 @@ fn broyden_case7() {
 
     rf.solve(&mut user_model);
 
-    let mut solution = nalgebra::DVector::zeros(problem_size);
-    solution[0] = -0.7304305192613078;
-    solution[1] = -0.7290279647590477;
-    solution[2] = -0.7264123904887514;
-    solution[3] = -0.7215373259970228;
-    solution[4] = -0.7124600171972677;
-    solution[5] = -0.6955898448220841;
-    solution[6] = -0.6643460752880278;
-    solution[7] = -0.60685803986969;
-    solution[8] = -0.5023562299431517;
-    solution[9] = -0.31662479035539975;
+    let solution = solution_broyden1965_case7();
 
-    assert_eq!(user_model.get_iteratives(), solution);
+    for i in 0..problem_size {
+        assert!(float_cmp::approx_eq!(
+            f64,
+            user_model.get_iteratives()[i],
+            solution[i],
+            epsilon = 1e-6
+        ));
+    }
 
 
 }
@@ -81,29 +84,16 @@ fn broyden_case8() {
 
     rf.solve(&mut user_model);
 
-    let mut solution = nalgebra::DVector::zeros(problem_size);
-    solution[0] = -0.7320476414628936;
-    solution[1] = -0.7320448995371868;
-    solution[2] = -0.7320397830403825;
-    solution[3] = -0.7320302355490265;
-    solution[4] = -0.7320124197630306;
-    solution[5] = -0.7319791753163778;
-    solution[6] = -0.7319171412487779;
-    solution[7] = -0.7318013872866127;
-    solution[8] = -0.7315853985385721;
-    solution[9] = -0.7311823966465685;
-    solution[10] = -0.7304305192613078;
-    solution[11] = -0.7290279647590477;
-    solution[12] = -0.7264123904887516;
-    solution[13] = -0.721537325997023;
-    solution[14] = -0.7124600171972679;
-    solution[15] = -0.6955898448220843;
-    solution[16] = -0.6643460752880272;
-    solution[17] = -0.6068580398696893;
-    solution[18] = -0.5023562299431515;
-    solution[19] = -0.31662479035539975;
+    let solution = solution_broyden1965_case8();
 
-    assert_eq!(user_model.get_iteratives(), solution);
+    for i in 0..problem_size {
+        assert!(float_cmp::approx_eq!(
+            f64,
+            user_model.get_iteratives()[i],
+            solution[i],
+            epsilon = 1e-6
+        ));
+    }
 
 
 }
@@ -117,9 +107,7 @@ fn broyden_case9() {
 
     rf.solve(&mut user_model);
 
-    let mut solution = nalgebra::DVector::zeros(problem_size);
-    solution[0] = 1.0;
-    solution[1] = 1.0;
+    let solution = solution_broyden1965_case9();
 
     for i in 0..problem_size {
         assert!(float_cmp::approx_eq!(
@@ -142,9 +130,7 @@ fn broyden_case10() {
 
     rf.solve(&mut user_model);
 
-    let mut solution = nalgebra::DVector::zeros(problem_size);
-    solution[0] = 5.0;
-    solution[1] = 4.0;
+    let solution = solution_broyden1965_case10();
 
 
     for i in 0..problem_size {
